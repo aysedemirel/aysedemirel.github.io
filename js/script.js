870de4b0cdb9d91c00ca5073e908959fea683e98
Ayşe Demirel <aysedemirel1996@gmail.com>
2021-04-24 00:28:52 +0300

    Update about page(Add summary)

--- js/script.js
+++ js/script.js
@@ -1,41 +1,52 @@
 // DARK MODE
-const chk = document.getElementById('chk');
+const chk = document.getElementById("chk");
 
-chk.addEventListener('change', () => {
-	document.body.classList.toggle('normal');
+chk.addEventListener("change", () => {
+  document.body.classList.toggle("normal");
 });
 
 //CHART PART
-google.charts.load('current', {'packages':['corechart']});
+google.charts.load("current", { packages: ["corechart"] });
 google.charts.setOnLoadCallback(drawChart);
 
 // Draw the chart and set the chart values
 function drawChart() {
   var data = google.visualization.arrayToDataTable([
-  ['Blog', 'Percentage'],
-  ['Java', 8],
-  ['Web', 2],
-  ['Python', 4],
-  ['OOP', 2],
-  ['Clean code', 8]
-	]);
+    ["Blog", "Percentage"],
+    ["Java", 8],
+    ["Web", 2],
+    ["Python", 4],
+    ["OOP", 2],
+    ["Clean code", 8],
+  ]);
 
   // Optional; add a title and set the width and height of the chart
-  var options = {legend: 'none', pieSliceText: 'label', 'width':550, 'height':400, 'backgroundColor':{fill:'transparent'},  is3D: true};
+  var options = {
+    legend: "none",
+    pieSliceText: "label",
+    width: 550,
+    height: 400,
+    backgroundColor: { fill: "transparent" },
+    is3D: true,
+  };
 
   // Display the chart inside the <div> element with id="piechart"
-  var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+  var chart = new google.visualization.PieChart(
+    document.getElementById("piechart")
+  );
   chart.draw(data, options);
-  google.visualization.events.addListener(chart, 'select', selectHandler);
+  google.visualization.events.addListener(chart, "select", selectHandler);
 }
 
 function selectHandler(e) {
-  alert('A table row was selected');
+  alert("A table row was selected");
 }
 
 // SOCIAL MEDIA PART
 var social = document.getElementById("link-list");
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {
+  scrollFunction();
+};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
