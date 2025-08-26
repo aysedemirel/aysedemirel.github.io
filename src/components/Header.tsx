ee00a71efd73a55a590800038875a504b50984c0
Ayşe Demirel Deniz <aysedemirel1996@gmail.com>
2025-08-26 18:45:16 +0500

    Add basic structure

---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+import { Header as AntHeader } from 'antd/es/layout/layout';
+
+const Header = () => {
+  return <AntHeader className="layout-header"></AntHeader>;
+};
+
+export default Header;
---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,29 @@
+import opps from '../assets/img/opps.jpg';
+import { Button } from 'antd';
+import { useTranslation } from 'react-i18next';
+import Footer from '../components/Footer';
+import Header from '../components/Header';
+
+const NotFound = () => {
+  const { t } = useTranslation();
+  return (
+    <div className="container">
+      <section className="first-part">
+        <Header />
+      </section>
+      <div className="not-found-container">
+        <div className="not-found-txt">
+          <h1>{t('oooppss')}</h1>
+          <p>{t('oooppss-txt')}</p>
+          <Button className="not-found-btn" onClick={() => window.open('/', '_self')}>
+            {t('go-back-to-home')}
+          </Button>
+        </div>
+        <img src={opps} className="not-found-img" />
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default NotFound;
